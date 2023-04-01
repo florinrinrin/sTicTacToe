2138453d281fccf15c3fdec2aad99023442868b1
florinrinrin <neagraflorin@yahoo.com>
2023-04-01 20:24:18 +0300

    added printGrid method

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 public class App {
 
     public static void main(String[] args) {
+    char[][] grid = {};
+    AppFunctionalities appFunctionalities = new AppFunctionalities();
+    appFunctionalities.initializeGrid(grid);
 
     }
 }
--- src/AppFunctionalities.java
+++ src/AppFunctionalities.java
@@ -2,10 +2,22 @@ import java.util.Arrays;
 
 public class AppFunctionalities {
 
-    public void initializeGrid (String[][] grid){
-        grid = new String[][]{{" "," "," "},
-                              {" "," "," "},
-                              {" "," "," "}};
-        System.out.println(Arrays.deepToString(grid));
+    public void initializeGrid (char[][] grid){
+        System.out.println("---------");
+        grid = new char[][]{{'|','_','_','_','|'},
+                            {'|','_','_','_','|'},
+                            {'|','_','_','_','|'}};
+
+        printGrid(grid);
+    }
+
+    private static void printGrid(char[][] grid) {
+        for (char[] row : grid){
+            for (char c : row){
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+        System.out.print("---------");
     }
 }
